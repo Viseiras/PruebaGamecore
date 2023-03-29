fd52c1d00fefe395f872b32cd1de206ac53d49e7
Viseiras <javi.vice@gmail.com>
2023-03-29 21:09:27 +0200

    Conectar con la API

--- app/src/main/java/com/example/pruebagamecore/MainActivity.java
+++ app/src/main/java/com/example/pruebagamecore/MainActivity.java
@@ -20,61 +20,39 @@ public class MainActivity extends AppCompatActivity {
 
     public static void main(String[] args)
     {
-        OkHttpClient client = new OkHttpClient();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.rawg.io/api/games/3498?key=[TU_API_KEY]";
 
-        String url = "https://api.rawg.io/api/games/3498?key=[fb8206eba55f4d3694c9bd5c8dc7391f]";
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Analiza la respuesta JSON y extrae los datos que necesitas
+                        String title = response.getString("name");
+                        String imageUrl = response.getJSONObject("background_image").getString("original");
 
-        try
-        {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                String responseBody = response.body().string();
-                // do something with the response body
-            } else {
-                // handle error
-            }
-        }
+                        // Usa Glide para cargar la imagen en una ImageView
+                        ImageView imageView = findViewById(R.id.imageView);
+                        Glide.with(MainActivity.this).load(imageUrl).into(imageView);
 
-        catch (IOException e) {
-            // handle exception
-        }
+                        // Actualiza la vista con el título del juego
+                        TextView textView = findViewById(R.id.textView);
+                        textView.setText(title);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Maneja errores de la solicitud
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
     }
 
 
 
-    /*
-    RequestQueue queue = Volley.newRequestQueue(this);
-    String url = "https://api.rawg.io/api/games/3498?key=[TU_API_KEY]";
 
-    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
-                @Override
-                public void onResponse(JSONObject response) {
-                    // Analiza la respuesta JSON y extrae los datos que necesitas
-                    String title = response.getString("name");
-                    String imageUrl = response.getJSONObject("background_image").getString("original");
-
-                    // Usa Glide para cargar la imagen en una ImageView
-                    ImageView imageView = findViewById(R.id.imageView);
-                    Glide.with(MainActivity.this).load(imageUrl).into(imageView);
-
-                    // Actualiza la vista con el título del juego
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(title);
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    // Maneja errores de la solicitud
-                }
-            });
-
-queue.add(jsonObjectRequest);
-*/
 }
